265a2a478b2b364ba52b7155fbd70c42da955e01
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-18 21:38:47 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import './App.css'
+import "./App.css";
+import DaisyNav from "./components/daisynav/DaisyNav";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-
   return (
     <>
-      <section>
-      <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
-      </section>
+      {/* <DaisyNav></DaisyNav> */}
+      <Navbar></Navbar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
